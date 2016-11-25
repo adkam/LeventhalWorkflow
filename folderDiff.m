8e7531753b2a6ba0b0ac174667bd0fdb1f83dd83
adkam <adkamath@umich.edu>
2016-11-25 11:24:36 -0500

    Create folderDiff.m

---
+++ folderDiff.m
@@ -0,0 +1,7 @@
+%For full paths, visdiff('C:\Work\comp\folder1', 'C:\Work\comp\folder2')
+
+%Opens comparison tool on Matlab, as detailed on 
+%https://www.mathworks.com/help/matlab/matlab_env/comparing-files-and-folders.html#brqxeeu-111
+
+visdiff(folder1, folder2)
+end
